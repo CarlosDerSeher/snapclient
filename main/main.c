78d8d66a38bd2e01a938b88ca4eb4770f1de5519
Manuel Blatt <manuel.blatt@web.de>
2025-11-25 21:21:09 +0100

    setup_network function

--- main/main.c
+++ main/main.c
@@ -1152,6 +1152,185 @@ int process_data(
 }
 
 
+void setup_network(esp_netif_t** netif) {
+  int rc1, rc2 = ERR_OK;
+  mdns_result_t *r;
+  esp_err_t err = 0;
+  uint16_t remotePort = 0;
+
+  while (1) {
+  
+    if (lwipNetconn != NULL) {
+      netconn_delete(lwipNetconn);
+      lwipNetconn = NULL;
+    }
+  
+    ESP_LOGI(TAG, "Wait for network connection");
+#if CONFIG_SNAPCLIENT_USE_INTERNAL_ETHERNET || \
+    CONFIG_SNAPCLIENT_USE_SPI_ETHERNET
+    esp_netif_t *eth_netif =
+        network_get_netif_from_desc(NETWORK_INTERFACE_DESC_ETH);
+#endif
+    esp_netif_t *sta_netif =
+        network_get_netif_from_desc(NETWORK_INTERFACE_DESC_STA);
+    while (1) {
+#if CONFIG_SNAPCLIENT_USE_INTERNAL_ETHERNET || \
+    CONFIG_SNAPCLIENT_USE_SPI_ETHERNET
+      bool ethUp = network_is_netif_up(eth_netif);
+
+      if (ethUp) {
+        netif = eth_netif;
+
+        break;
+      }
+#endif
+
+      bool staUp = network_is_netif_up(sta_netif);
+      if (staUp) {
+        *netif = sta_netif;
+
+        break;
+      }
+
+      vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+
+#if SNAPCAST_SERVER_USE_MDNS
+    // Find snapcast server
+    // Connect to first snapcast server found
+    r = NULL;
+    err = 0;
+    while (!r || err) {
+      ESP_LOGI(TAG, "Lookup snapcast service on network");
+      esp_err_t err = mdns_query_ptr("_snapcast", "_tcp", 3000, 20, &r);
+      if (err) {
+        ESP_LOGE(TAG, "Query Failed");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+      }
+
+      if (!r) {
+        ESP_LOGW(TAG, "No results found!");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+      }
+    }
+
+    ESP_LOGI(TAG, "\n~~~~~~~~~~ MDNS Query success ~~~~~~~~~~");
+    mdns_print_results(r);
+    ESP_LOGI(TAG, "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+
+    ip_addr_t remote_ip;
+    mdns_result_t *re = r;
+    while (re) {
+      mdns_ip_addr_t *a = re->addr;
+#if CONFIG_SNAPCLIENT_CONNECT_IPV6
+      if (a->addr.type == IPADDR_TYPE_V6) {
+        *netif = re->esp_netif;
+        break;
+      }
+
+      // TODO: fall back to IPv4 if no IPv6 was available
+#else
+      if (a->addr.type == IPADDR_TYPE_V4) {
+        *netif = re->esp_netif;
+        break;
+      }
+#endif
+
+      re = re->next;
+    }
+
+    if (!re) {
+      mdns_query_results_free(r);
+
+      ESP_LOGW(TAG, "didn't find any valid IP in MDNS query");
+
+      continue;
+    }
+
+    ip_addr_copy(remote_ip, re->addr->addr);
+    remotePort = r->port;
+
+    mdns_query_results_free(r);
+
+    ESP_LOGI(TAG, "Found %s:%d", ipaddr_ntoa(&remote_ip), remotePort);
+#else
+    ip_addr_t remote_ip;
+
+    if (ipaddr_aton(SNAPCAST_SERVER_HOST, &remote_ip) == 0) {
+      ESP_LOGE(TAG, "can't convert static server adress to numeric");
+      continue;
+    }
+
+    remotePort = SNAPCAST_SERVER_PORT;
+
+    ESP_LOGI(TAG, "try connecting to static configuration %s:%d",
+             ipaddr_ntoa(&remote_ip), remotePort);
+#endif
+
+    if (remote_ip.type == IPADDR_TYPE_V4) {
+      lwipNetconn = netconn_new(NETCONN_TCP);
+
+      ESP_LOGV(TAG, "netconn using IPv4");
+    } else if (remote_ip.type == IPADDR_TYPE_V6) {
+      lwipNetconn = netconn_new(NETCONN_TCP_IPV6);
+
+      ESP_LOGV(TAG, "netconn using IPv6");
+    } else {
+      ESP_LOGW(TAG, "remote IP has unsupported IP type");
+
+      continue;
+    }
+
+    if (lwipNetconn == NULL) {
+      ESP_LOGE(TAG, "can't create netconn");
+
+      continue;
+    }
+
+#define USE_INTERFACE_BIND
+
+#ifdef USE_INTERFACE_BIND  // use interface to bind connection
+    uint8_t netifIdx = esp_netif_get_netif_impl_index(*netif);
+    rc1 = netconn_bind_if(lwipNetconn, netifIdx);
+    if (rc1 != ERR_OK) {
+      ESP_LOGE(TAG, "can't bind interface %s", network_get_ifkey(*netif));
+    }
+#else  // use IP to bind connection
+    if (remote_ip.type == IPADDR_TYPE_V4) {
+      // rc1 = netconn_bind(lwipNetconn, &ipAddr, 0);
+      rc1 = netconn_bind(lwipNetconn, IP4_ADDR_ANY, 0);
+    } else {
+      rc1 = netconn_bind(lwipNetconn, IP6_ADDR_ANY, 0);
+    }
+
+    if (rc1 != ERR_OK) {
+      ESP_LOGE(TAG, "can't bind local IP");
+    }
+#endif
+    rc2 = netconn_connect(lwipNetconn, &remote_ip, remotePort);
+    if (rc2 != ERR_OK) {
+      ESP_LOGE(TAG, "can't connect to remote %s:%d, err %d",
+               ipaddr_ntoa(&remote_ip), remotePort, rc2);
+
+#if !SNAPCAST_SERVER_USE_MDNS
+      vTaskDelay(pdMS_TO_TICKS(1000));
+#endif
+    }
+
+    if (rc1 != ERR_OK || rc2 != ERR_OK) {
+      netconn_close(lwipNetconn);
+      netconn_delete(lwipNetconn);
+      lwipNetconn = NULL;
+
+      continue;
+    }
+
+    ESP_LOGI(TAG, "netconn connected using %s", network_get_ifkey(*netif));
+    break; // SUCCESS
+  }
+}
+
+
 /**
  *
  */
@@ -1166,14 +1345,11 @@ static void http_get_task(void *pvParameters) {
   time_message_t time_message_rx = {{0, 0}};
   time_sync_data.lastTimeSync = 0;
   time_sync_data.timeSyncMessageTimer = NULL;
-  esp_err_t err = 0;
   bool received_codec_header = false;
-  mdns_result_t *r;
   codec_type_t codec = NONE;
   snapcastSetting_t scSet;
   pcm_chunk_message_t *pcmData = NULL;
-  uint16_t remotePort = 0;
-  int rc1 = ERR_OK, rc2 = ERR_OK;
+  int rc1 = ERR_OK;
   struct netbuf *firstNetBuf = NULL;
   uint16_t len;
   time_sync_data.timeout = FAST_SYNC_LATENCY_BUF;
@@ -1245,173 +1421,11 @@ static void http_get_task(void *pvParameters) {
         serverSettingsString = NULL;
       }
 
-      if (lwipNetconn != NULL) {
-        netconn_delete(lwipNetconn);
-        lwipNetconn = NULL;
-      }
     }
 
-    ESP_LOGI(TAG, "Wait for network connection");
-#if CONFIG_SNAPCLIENT_USE_INTERNAL_ETHERNET || \
-    CONFIG_SNAPCLIENT_USE_SPI_ETHERNET
-    esp_netif_t *eth_netif =
-        network_get_netif_from_desc(NETWORK_INTERFACE_DESC_ETH);
-#endif
-    esp_netif_t *sta_netif =
-        network_get_netif_from_desc(NETWORK_INTERFACE_DESC_STA);
-    while (1) {
-#if CONFIG_SNAPCLIENT_USE_INTERNAL_ETHERNET || \
-    CONFIG_SNAPCLIENT_USE_SPI_ETHERNET
-      bool ethUp = network_is_netif_up(eth_netif);
-
-      if (ethUp) {
-        netif = eth_netif;
-
-        break;
-      }
-#endif
-
-      bool staUp = network_is_netif_up(sta_netif);
-      if (staUp) {
-        netif = sta_netif;
-
-        break;
-      }
-
-      vTaskDelay(pdMS_TO_TICKS(1000));
-    }
-
-#if SNAPCAST_SERVER_USE_MDNS
-    // Find snapcast server
-    // Connect to first snapcast server found
-    r = NULL;
-    err = 0;
-    while (!r || err) {
-      ESP_LOGI(TAG, "Lookup snapcast service on network");
-      esp_err_t err = mdns_query_ptr("_snapcast", "_tcp", 3000, 20, &r);
-      if (err) {
-        ESP_LOGE(TAG, "Query Failed");
-        vTaskDelay(pdMS_TO_TICKS(1000));
-      }
-
-      if (!r) {
-        ESP_LOGW(TAG, "No results found!");
-        vTaskDelay(pdMS_TO_TICKS(1000));
-      }
-    }
-
-    ESP_LOGI(TAG, "\n~~~~~~~~~~ MDNS Query success ~~~~~~~~~~");
-    mdns_print_results(r);
-    ESP_LOGI(TAG, "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-
-    ip_addr_t remote_ip;
-    mdns_result_t *re = r;
-    while (re) {
-      mdns_ip_addr_t *a = re->addr;
-#if CONFIG_SNAPCLIENT_CONNECT_IPV6
-      if (a->addr.type == IPADDR_TYPE_V6) {
-        netif = re->esp_netif;
-        break;
-      }
-
-      // TODO: fall back to IPv4 if no IPv6 was available
-#else
-      if (a->addr.type == IPADDR_TYPE_V4) {
-        netif = re->esp_netif;
-        break;
-      }
-#endif
-
-      re = re->next;
-    }
-
-    if (!re) {
-      mdns_query_results_free(r);
-
-      ESP_LOGW(TAG, "didn't find any valid IP in MDNS query");
-
-      continue;
-    }
-
-    ip_addr_copy(remote_ip, re->addr->addr);
-    remotePort = r->port;
-
-    mdns_query_results_free(r);
-
-    ESP_LOGI(TAG, "Found %s:%d", ipaddr_ntoa(&remote_ip), remotePort);
-#else
-    ip_addr_t remote_ip;
-
-    if (ipaddr_aton(SNAPCAST_SERVER_HOST, &remote_ip) == 0) {
-      ESP_LOGE(TAG, "can't convert static server adress to numeric");
-      continue;
-    }
-
-    remotePort = SNAPCAST_SERVER_PORT;
-
-    ESP_LOGI(TAG, "try connecting to static configuration %s:%d",
-             ipaddr_ntoa(&remote_ip), remotePort);
-#endif
-
-    if (remote_ip.type == IPADDR_TYPE_V4) {
-      lwipNetconn = netconn_new(NETCONN_TCP);
-
-      ESP_LOGV(TAG, "netconn using IPv4");
-    } else if (remote_ip.type == IPADDR_TYPE_V6) {
-      lwipNetconn = netconn_new(NETCONN_TCP_IPV6);
-
-      ESP_LOGV(TAG, "netconn using IPv6");
-    } else {
-      ESP_LOGW(TAG, "remote IP has unsupported IP type");
-
-      continue;
-    }
-
-    if (lwipNetconn == NULL) {
-      ESP_LOGE(TAG, "can't create netconn");
-
-      continue;
-    }
-
-#define USE_INTERFACE_BIND
-
-#ifdef USE_INTERFACE_BIND  // use interface to bind connection
-    uint8_t netifIdx = esp_netif_get_netif_impl_index(netif);
-    rc1 = netconn_bind_if(lwipNetconn, netifIdx);
-    if (rc1 != ERR_OK) {
-      ESP_LOGE(TAG, "can't bind interface %s", network_get_ifkey(netif));
-    }
-#else  // use IP to bind connection
-    if (remote_ip.type == IPADDR_TYPE_V4) {
-      // rc1 = netconn_bind(lwipNetconn, &ipAddr, 0);
-      rc1 = netconn_bind(lwipNetconn, IP4_ADDR_ANY, 0);
-    } else {
-      rc1 = netconn_bind(lwipNetconn, IP6_ADDR_ANY, 0);
-    }
-
-    if (rc1 != ERR_OK) {
-      ESP_LOGE(TAG, "can't bind local IP");
-    }
-#endif
-    rc2 = netconn_connect(lwipNetconn, &remote_ip, remotePort);
-    if (rc2 != ERR_OK) {
-      ESP_LOGE(TAG, "can't connect to remote %s:%d, err %d",
-               ipaddr_ntoa(&remote_ip), remotePort, rc2);
-
-#if !SNAPCAST_SERVER_USE_MDNS
-      vTaskDelay(pdMS_TO_TICKS(1000));
-#endif
-    }
-
-    if (rc1 != ERR_OK || rc2 != ERR_OK) {
-      netconn_close(lwipNetconn);
-      netconn_delete(lwipNetconn);
-      lwipNetconn = NULL;
-
-      continue;
-    }
-
-    ESP_LOGI(TAG, "netconn connected using %s", network_get_ifkey(netif));
+    // NETWORK setup ends here ( or before getting mac address )
+    setup_network(&netif);
+ 
 
     if (reset_latency_buffer() < 0) {
       ESP_LOGE(TAG,
